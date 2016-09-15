fbb885fe219916ea3ccbce93034ad9526199a23b
App Academy Student <student@appacademy.io>
2016-09-15 13:44:33 -0700

    fix method_missing and parse_pos and board grid on final level

--- board.rb
+++ board.rb
@@ -10,6 +10,7 @@ class Board
   end
 
   def self.from_file(filename)
+    # byebug
     rows = File.readlines(filename).map(&:chomp)
     tiles = rows.map do |row|
       nums = row.split("").map { |char| Integer(char) }
@@ -53,7 +54,8 @@ class Board
   alias_method :rows, :size
 
   def solved?
-    rows.all? { |row| solved_set?(row) } &&
+    # byebug
+    grid.all? { |row| solved_set?(row) } &&
       columns.all? { |col| solved_set?(col) } &&
       squares.all? { |square| solved_set?(square) }
   end
